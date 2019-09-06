cd7123888fa5f591e2127f80e7665ef8d760138b
Caio Norbiato <caio@norbiato.net>
2019-09-05 23:35:11 -0300

    Tarefa Aula

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import { headingH1 } from './headingH1';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 
 class App extends Component {
 
+  state = {
+    inputValue: ''
+  }
+
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
   render() {
+
+    const { newValue, clickButton } = this.props;
+    const { inputValue } = this.state;
+
     return (
       <div className="App" style={{ paddingTop: '10px' }}>
-        <input type='text' />
-        <button>
-          Click me!
+        <input type='text' onChange={this.inputChange} value={inputValue} />
+        <button onClick={() => clickButton(inputValue)}>
+          Click!
         </button>
-        <h1>texto</h1>
+        <headingH1 variavel={newValue} />
       </div>
     );
   }
-}
-export default App;
+};
+
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickButton }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
